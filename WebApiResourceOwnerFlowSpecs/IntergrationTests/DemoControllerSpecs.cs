30ba7c590c4deb6c0c50e015f8b3d3359e84b846
Maurice de Beijer <mauricedb@computer.org>
2015-02-03 21:30:00 +0100

    Added unit tests

---
+++ WebApiResourceOwnerFlowSpecs/IntergrationTests/DemoControllerSpecs.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Owin.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApiResourceOwnerFlow;
+
+namespace WebApiResourceOwnerFlowSpecs.IntergrationTests
+{
+    [TestClass]
+    public class DemoControllerSpecs
+    {
+        [TestMethod]
+        public async Task Get_without_authorization_header_should_return_unauthorized()
+        {
+            using (var server = TestServer.Create<Startup>())
+            {
+                var response = await server.HttpClient.GetAsync("/api/demo/");
+
+                response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+            }
+        }
+
+        [TestMethod]
+        public async Task Get_with_invalid_authorization_header_should_return_unauthorized()
+        {
+            using (var server = TestServer.Create<Startup>())
+            {
+                var client = server.HttpClient;
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "invalidtoken");
+
+                var response = await client.GetAsync("/api/demo/");
+                
+                response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+            }
+        }
+
+        [TestMethod]
+        public async Task Get_with_valid_user_credentials_should_return_message()
+        {
+            using (var server = TestServer.Create<Startup>())
+            {
+                var httpClient = server.HttpClient;
+                await httpClient.AuthorizeWith("maurice", "pass");
+
+                var response = await httpClient.GetStringAsync("/api/demo/");
+
+                response.Should().Be("\"The user is: maurice\"");
+            }
+        }
+
+        [TestMethod]
+        public async Task Get_with_invalid_user_credentials_should_return_message()
+        {
+            using (var server = TestServer.Create<Startup>())
+            {
+                var httpClient = server.HttpClient;
+
+                await httpClient.AuthorizeWith("nouser", "nopass");
+
+                Func<Task> request = async () => await httpClient.GetStringAsync("/api/demo/");
+                request.ShouldThrow<HttpRequestException>()
+                    .And.Message.Should().Contain("401 (Unauthorized)");
+            }
+        }
+    }
+}
---
+++ WebApiResourceOwnerFlowSpecs/IntergrationTests/HttpClientExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace WebApiResourceOwnerFlowSpecs.IntergrationTests
+{
+    public static class HttpClientExtensions
+    {
+        public static async Task AuthorizeWith(this HttpClient httpClient, string username, string password)
+        {
+            var tokenDetails = new[]
+            {
+                new KeyValuePair<string, string>("grant_type", "password"),
+                new KeyValuePair<string, string>("username", username),
+                new KeyValuePair<string, string>("password", password)
+            };
+
+            var tokenPostData = new FormUrlEncodedContent(tokenDetails);
+            var tokenResult = await httpClient.PostAsync("/token", tokenPostData);
+
+            if (tokenResult.IsSuccessStatusCode)
+            {
+                var body = JObject.Parse(await tokenResult.Content.ReadAsStringAsync());
+                var token = (string) body["access_token"];
+
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+        }
+    }
+}
