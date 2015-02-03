0d786be746c012bb9a051e460556ff1f2e525189
Maurice de Beijer <mauricedb@computer.org>
2015-02-03 17:11:52 +0100

    Added unit tests

---
+++ WebApiResourceOwnerFlowSpecs/UnitTests/DemoControllerSpecs.cs
@@ -0,0 +1,41 @@
+﻿using System.Security.Claims;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApiResourceOwnerFlow;
+
+namespace WebApiResourceOwnerFlowSpecs.UnitTests
+{
+    [TestClass]
+    public class DemoControllerSpecs
+    {
+        [TestMethod]
+        public void Get_should_return_anonymous()
+        {
+            var controller = new DemoController
+            {
+                User = new ClaimsPrincipal()
+            };
+
+            var response = controller.Get();
+
+            response.Should().Be(@"The user is: anonymous");
+        }
+
+        [TestMethod]
+        public void Get_should_return_maurice()
+        {
+            var controller = new DemoController
+            {
+                User = new ClaimsPrincipal(
+                    new ClaimsIdentity(new[]
+                    {
+                        new Claim(ClaimTypes.Name, "Maurice")
+                    }))
+            };
+
+            var response = controller.Get();
+
+            response.Should().Be(@"The user is: Maurice");
+        }
+    }
+}
